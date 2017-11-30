21cc7113023d90961b27331894c8602013fc4d8d
Mark Bello <mcbello51286@gmail.com>
2017-11-30 15:19:13 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    results = []
+    while i < collection.length
+      if yield(collection[i])
+        results.push(collection[i])
+      end
+      i += 1
+    end
+    results
+  else
+    puts “什么什么什么”
+  end
 end
