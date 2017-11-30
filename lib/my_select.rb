b726f17525745f1c7a6e78930e7bb987a1d7d649
Mark Bello <mcbello51286@gmail.com>
2017-11-30 15:06:44 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    results = []
+    while i < collection.length
+      results.push(yield(collection[i]))
+      i += 1
+    end
+    results
+  else
+    puts “什么什么什么”
+  end
 end
